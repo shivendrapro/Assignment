13fc7339658221a32d4f872db37bc3ca3777a1d7
shivendrapro0 <shivendrapro0@gmail.com>
2014-10-06 18:37:59 +0530

    Updating

--- chef-repo/cookbooks/deployment/attributes/default.rb
+++ chef-repo/cookbooks/deployment/attributes/default.rb
@@ -2,5 +2,3 @@
 default['deployment']['USER'] = "root"
 default['deployment']['DOCROOT'] = "/etc/httpd/htdocs"
 default['deployment']['JENKINSURL'] = "http://delvmplmmedtr03.sapient.com:8080/jenkins/job/ARTIFACT_PRJ"
-
-
